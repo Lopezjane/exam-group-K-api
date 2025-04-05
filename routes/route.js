4d7f0b81e2534f9ae219813ebdaa82e9216b0094
unknown <YY527@ccs.local>
2025-04-05 14:42:54 +0800

    initial commit

---
+++ routes/route.js
@@ -0,0 +1,10 @@
+route.js
+import { Router } from 'express';
+
+export const router = Router();
+
+let exams = [
+  { id: 1, name: 'Math Exam', date: '2025-04-05' },
+  { id: 2, name: 'Science Exam', date: '2025-04-05' }
+];
+
